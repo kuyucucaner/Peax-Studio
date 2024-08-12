f0cba6402b050ea54cc0e775cb812ca82c646e62
kuyucucaner <tahacanerkuyucu@gmail.com>
2024-08-12 11:47:37 +0300

    project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar'
 import Home from './pages/home';
 import About from './pages/about';
-import Navbar from './components/navbar'
+import Project from './pages/project'
 import './styles/scroll-bar.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about-us" element={<About />} />
+            <Route path="/projects" element={<Project />} />
           </Routes>
         </main>
       </div>
---
+++ frontend/src/pages/project.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "../styles/project.css";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <main className="project-container">
+      <h1 className="project-title">Projects</h1>
+
+      <div className="project-section">
+        <div className="project-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmjuXJImvY1gN20U5SxJ34RIyc9d1EBnnb2Q&s"
+            alt="Project 1"
+            className="project-image"
+          />
+          <div className="project-content">
+            <h2 className="project-subtitle">Project Title 1</h2>
+            <p className="project-description">
+              Brief description of the project, highlighting key features and
+              outcomes. Brief description of the project, highlighting key
+              features and outcomes. Brief description of the project,
+              highlighting key features and outcomes. Brief description of the
+              project, highlighting key features and outcomes. Brief description
+              of the project, highlighting key features and outcomes.
+            </p>
+            <Link href="#" className="project-link">
+              View Project
+            </Link>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmjuXJImvY1gN20U5SxJ34RIyc9d1EBnnb2Q&s"
+            alt="Project 2"
+            className="project-image"
+          />
+          <div className="project-content">
+            <h2 className="project-subtitle">Project Title 2</h2>
+            <p className="project-description">
+            Brief description of the project, highlighting key features and
+              outcomes. Brief description of the project, highlighting key
+              features and outcomes. Brief description of the project,
+              highlighting key features and outcomes. Brief description of the
+              project, highlighting key features and outcomes. Brief description
+              of the project, highlighting key features and outcomes.
+            </p>
+            <Link href="#" className="project-link">
+              View Project
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
