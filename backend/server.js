60547d99c9b79b7a35399b150b94d6f43902952c
kuyucucaner <tahacanerkuyucu@gmail.com>
2024-09-01 15:43:50 +0300

    felek

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('tiny'));
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 // Port Ayarlama ve Sunucuyu Başlatma
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Sunucu ${PORT} portunda çalışıyor`));
