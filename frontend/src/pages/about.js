63ab9fe5b3d1760213f6fa74a3050c6de33f1790
kuyucucaner <tahacanerkuyucu@gmail.com>
2024-09-02 11:45:12 +0300

    update some css

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "../styles/about.css";
+import MissionImage from "../images/mission.png";
+import AboutHeaderImage from "../images/aboutheader.webp";
+import VisionImage from "../images/vision.jpg";
+import ValueImage from "../images/value.jpg";
 
 const About = () => {
   return (
     <section className="about-section" id="about-section">
       <div className="about-box">
+        <img
+          src={AboutHeaderImage}
+          alt="Peax Studio Creative Design"
+          className="about-image"
+        />
         <div className="about-content">
           <h1 className="about-title">About Peax Studio</h1>
           <p className="about-description">
@@ -21,17 +30,29 @@ const About = () => {
             every detail aligns with our clients' vision and goals. We are not
             just a studio; we are your partners in success. Join us on this
             journey to elevate your brand and create lasting impressions in the
-            digital world.
+            digital world. digital world. digital world. digital world.
+            journey to elevate your brand and create lasting impressions 
+            journey to elevate your brand and create lasting impressions  
+            journey to elevate your brand and create lasting impressions  
           </p>
-        <img
-          src="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283"
-          alt="Peax Studio Creative Design"
-          className="about-image"
-        />
         </div>
       </div>
+      <div className="about-sub-box">
+          <p className="about-description">
+            Our mission is to deliver innovative, efficient solutions that go
+            beyond merely meeting client expectations. We are driven by a
+            commitment to excellence, seeking to transform challenges into
+            opportunities through creative vision and the latest technological
+            advancements. Each project we undertake is an opportunity to set a
+            new standard of success, where our dedication to quality and detail
+            shines through.
+          </p>
+        </div>
+
+
       <div className="about-achievements">
         <div className="about-x">
+          <div className="about-text-section">
           <h2 className="about-subtitle">What Drives Us</h2>
           <p className="about-description">
             Our mission is to deliver innovative, efficient solutions that go
@@ -41,41 +62,24 @@ const About = () => {
             advancements. Each project we undertake is an opportunity to set a
             new standard of success, where our dedication to quality and detail
             shines through. At the heart of our mission is the belief in
-            building lasting relationships with our clients, founded on the
-            principles of trust, integrity, and mutual respect. We view every
-            client interaction as a chance to deepen these values and create
-            solutions that truly resonate.
+            building lasting relationships with our client.
           </p>
+          </div>
           <img
-            src="https://www.myhubintranet.com/wp-content/uploads/2021/01/develop-company-vision.jpg"
+            src={MissionImage}
             alt="Vision Illustration"
             className="about-subimage"
           ></img>
         </div>
 
         <div className="about-x">
-          <h2 className="about-subtitle">What Drives Us</h2>
-          <p className="about-description">
-            Our mission is to deliver innovative, efficient solutions that go
-            beyond merely meeting client expectations. We are driven by a
-            commitment to excellence, seeking to transform challenges into
-            opportunities through creative vision and the latest technological
-            advancements. Each project we undertake is an opportunity to set a
-            new standard of success, where our dedication to quality and detail
-            shines through. At the heart of our mission is the belief in
-            building lasting relationships with our clients, founded on the
-            principles of trust, integrity, and mutual respect. We view every
-            client interaction as a chance to deepen these values and create
-            solutions that truly resonate.
-          </p>
           <img
-            src="https://www.myhubintranet.com/wp-content/uploads/2021/01/develop-company-vision.jpg"
+            src={VisionImage}
             alt="Vision Illustration"
             className="about-subimage"
           ></img>
-        </div>
-        <div className="about-x">
-          <h2 className="about-subtitle">What Drives Us</h2>
+          <div className="about-text-section">
+          <h2 className="about-subtitle">Vision</h2>
           <p className="about-description">
             Our mission is to deliver innovative, efficient solutions that go
             beyond merely meeting client expectations. We are driven by a
@@ -89,8 +93,23 @@ const About = () => {
             client interaction as a chance to deepen these values and create
             solutions that truly resonate.
           </p>
+          </div>
+        </div>
+        <div className="about-x">
+          <div className="about-text-section">
+          <h2 className="about-subtitle">VALUE</h2>
+          <p className="about-description">
+            Our mission is to deliver innovative, efficient solutions that go
+            beyond merely meeting client expectations. We are driven by a
+            commitment to excellence, seeking to transform challenges into
+            opportunities through creative vision and the latest technological
+            advancements. Each project we undertake is an opportunity to set a
+            new standard of success, where our dedication to quality and detail
+            shines through. At the heart of our mission is the belief in
+          </p>
+          </div>
           <img
-            src="https://www.myhubintranet.com/wp-content/uploads/2021/01/develop-company-vision.jpg"
+            src={ValueImage}
             alt="Vision Illustration"
             className="about-subimage"
           ></img>
