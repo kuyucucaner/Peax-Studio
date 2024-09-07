45cccc7f26d87ab6eb307700bc509ca45684a791
kuyucucaner <tahacanerkuyucu@gmail.com>
2024-09-07 15:33:39 +0300

    dot

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      connectSrc: ["'self'", "https://peaxstudio-17bab99340d9.herokuapp.com",  "http://localhost:5000", , "http://www.peaxstudio.com"], // Güncellenmiş URL'ler
+      connectSrc: ["'self'", "https://peaxstudio-17bab99340d9.herokuapp.com",  "http://localhost:5000" , "http://www.peaxstudio.com"], // Güncellenmiş URL'ler
       scriptSrc: ["'self'", "https://cdnjs.cloudflare.com"],
       styleSrc: ["'self'", "https://fonts.googleapis.com", "https://stackpath.bootstrapcdn.com"],
       imgSrc: ["'self'", "data:"],
